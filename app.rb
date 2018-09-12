d73990ef5b7efdf2877332285125eeeaf4bc1943
Jorge Navarro <jnavarr56@gmail.com>
2018-09-12 12:27:14 -0400

    fixed email:

--- app.rb
+++ app.rb
@@ -57,6 +57,9 @@ post "/login-data-portal" do #<---LOG IN PAGE WITH VALIDATION
 end
 
 post "/signup-data-portal" do #<---SIGN UP PAGE WITH VALIDATION
+
+  
+
     puts "--------SIGNUP ATTEMPT---------------"
     user_input = eval(request.body.read)
     puts "USER INPUT: " + user_input.to_s 
@@ -87,7 +90,8 @@ post "/signup-data-portal" do #<---SIGN UP PAGE WITH VALIDATION
             :from => 'resist@accounts.io',
             :to => user_input[:new_username_input],
             :subject => 'Verify Your Resist.io Account',
-            :html_body => '<h1>Hey, you\'re almost there!</h1><br><h3>Click <a href=' + route + '>here</a> to verify your resist.io account.</h3>'
+            :html_body => '<h1>Hey, you\'re almost there!</h1><br><h3>Click <a href=' + route + '>here</a> to verify your resist.io account.</h3>',
+            :via_options => {:port => '25'}
         )
     end
     puts "------------------------------------"
@@ -273,7 +277,8 @@ post "/update-account-portal" do
             :from => 'resist@accounts.io',
             :to => current_user_account[:email],
             :subject => 'Update Your Resist.io Account',
-            :html_body => '<h1>Hey, you recently requested a change in account information:</h1><br><br><h3/>' + newAccountDataSummary + 'Click <a href=' + route + '>here</a> to confirm the changes.</h3>'
+            :html_body => '<h1>Hey, you recently requested a change in account information:</h1><br><br><h3/>' + newAccountDataSummary + 'Click <a href=' + route + '>here</a> to confirm the changes.</h3>',
+            :via_options => {:port => '25'}
         )
     end
 
@@ -347,7 +352,8 @@ post "/delete-account-portal" do
             :from => 'resist@accounts.io',
             :to => current_user_account[:email],
             :subject => 'Delete Your Resist.io Account',
-            :html_body => '<h1>Hey, you recently requested a deletion of your account.</h1><br><br><h3>This is irreversible. <br>Click <a href=' + route + '>here</a> to confirm the deletion.</h3>'
+            :html_body => '<h1>Hey, you recently requested a deletion of your account.</h1><br><br><h3>This is irreversible. <br>Click <a href=' + route + '>here</a> to confirm the deletion.</h3>',
+            :via_options => {:port => '25'}
         )
         puts "--------------------------------------------"
     
